fa4414906abeedf6266a24e3bf6e97efe420b0cd
Vinnie <34916@ma-web.nl>
2024-09-12 10:24:56 +0200

    made a card

---
+++ React/m9react/src/Card.jsx
@@ -0,0 +1,9 @@
+export function Card(){
+    return(
+        <div className="card">
+            <h2 className="card__title">Donkey Kong</h2>
+            <img src="https://pbs.twimg.com/profile_images/1510236306521088010/9my5V1ib_400x400.jpg" alt="" className="card__image" />
+            <p className="card__text">whatever whatever whateverwhatever whatever whatever whatever whatever whateverwhatever</p>
+        </div>
+    )
+}
