62647f5d322ac88f2407b08f80f7e013aa2d5e2c
Vinnie <34916@ma-web.nl>
2024-11-11 16:54:29 +0100

    adding move types

--- pokedex/src/Components/Pokedex/Pokedex.jsx
+++ pokedex/src/Components/Pokedex/Pokedex.jsx
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import DataContext from "../../hooks/context/DataContext";
 import Search from "../Search/Search";
 
 function Pokedex(){
-    const { data, id } = useContext(DataContext);
+    const { data, species, id } = useContext(DataContext);
+    const [moveTypes, setMoveTypes] = useState([]);
+    console.log(species);
 
     useEffect(() => {
         if(id){
@@ -22,13 +24,45 @@ function Pokedex(){
             <p className="pokedex__stats--item-number">{stat.base_stat}</p>
         </li>
     );
-    const movesSets = data?.moves?.map((move, index) => 
-        <li className="pokedex__moves--item" key={index}>
-            <p className="pokedex__moves--item-move">{move.move.name}</p>
-            <p className="pokedex__moves--item-number">{move.version_group_details[0].level_learned_at}</p>
-        </li>
+    useEffect(() => {
+        const fetchMoves = async () => {
+            if(data?.moves){
+                const moveData = data?.moves.map(move => 
+                    fetch(`https://pokeapi.co/api/v2/move/${move.move.name}`)
+                        .then((response) => response.json())
+                );
+                const getMoves = await Promise.all(moveData);
+                const getMove = getMoves.reduce((acc, move) => {
+                    acc[move.name] = move;
+                    return acc;
+                }, []);
+                setMoveTypes(getMove);
+            }
+        }
+        fetchMoves();
+    }, [data?.moves]);
+
+    const movesSets = data?.moves?.map((move, index) => {
+        const getMoveTypes = moveTypes[move.move.name] || [];
+        return(       
+            <li className="pokedex__moves--item" key={index}>
+                <p className="pokedex__moves--item-move">{move.move.name}</p>
+                <p className="pokedex__moves--item-number">{move.version_group_details[0].level_learned_at}</p>
+                <p className={`pokedex__moves--item-type pokedex__info--type-${getMoveTypes.type?.name}`}>{getMoveTypes.type?.name}</p>
+            </li>
+        );
+    } 
     );
-    return(
+    const descriptions = () => {
+        if(species?.flavor_text_entries){
+            const englishDescription = species.flavor_text_entries.find(
+                (description) => description.language.name === "en"
+            );
+            return englishDescription ? englishDescription.flavor_text.replace(/[\n\f]/g, " ")
+            : "no description";
+        }
+    };
+    return data && species ? (
         <section className="pokedex">
             <Search/>
             <div className="pokedex__wrapper">
@@ -37,8 +71,20 @@ function Pokedex(){
                         <img src={data?.sprites?.front_default} alt={data.name} className="pokedex__info--figure-image" />
                     </figure>
                     <article className="pokedex__info--wrapper">
-                        <h1 className="pokedex__info--name">{data.name}</h1>
+                        <span className="pokedex__info--names">
+                            <h1 className="pokedex__info--names-name">{data.name}</h1>
+                            <p className="pokedex__info--names-japanese">
+                                {
+                                    species.names?.find(
+                                        (name) => name.language.name === "ja-Hrkt"
+                                    )?.name
+                                }
+                            </p>
+                        </span>
                         <span className="pokedex__info--types">{typesSets}</span>
+                        <p className="pokedex__info--description">
+                            {descriptions()}
+                        </p>
                     </article>
                 </div>
                 <div className="pokedex__stats">
@@ -59,7 +105,8 @@ function Pokedex(){
                 </div>
             </div>
         </section>
-    );
+    ) : 
+    <>no pokemon whoomp whoomp</>;
 }
 
 export default Pokedex;
--- pokedex/src/hooks/context/DataContext.jsx
+++ pokedex/src/hooks/context/DataContext.jsx
@@ -4,20 +4,23 @@ const DataContext = createContext(null);
 
 function PokemonDataProvider({children}){
     const [data, setData] = useState([]);
+    const [species, setSpecies] = useState([]);
     const [id, setId] = useState(1);
 
     useEffect(() => {
         (async () => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setData(data);
-                })
+            
+            const [response, speciesResponse] = await Promise.all([
+                fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(response => response.json()),
+                fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(response => response.json())
+            ]);
+            setData(response);
+            setSpecies(speciesResponse);
         })();
     }, [id]);
 
     return(
-        <DataContext.Provider value={{data, id, setId}}>
+        <DataContext.Provider value={{data, species, id, setId}}>
             {children}
         </DataContext.Provider>
     )
